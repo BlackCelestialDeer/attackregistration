9a3a252e2ad7b4b5619b1f7718a340305bc923a5
BlackCelestialDeer <47210119+BlackCelestialDeer@users.noreply.github.com>
2025-04-30 11:30:38 +0200

    Revert "improvements"
    This reverts commit 86653336511f1b53b90a145ec58005bcadcc2582.

--- src/assets/ts/components/csDatabase.ts
+++ src/assets/ts/components/csDatabase.ts
@@ -1,13 +1,3 @@
 export class csDatabase {
-	public static attacksObject: Record<string, IAttackEntry[]> = {};
-
-	public saveAttack(date: Date, item: IAttackEntry): void {
-		const key = date.toDateString();
-		if (!csDatabase.attacksObject[key]) {
-			csDatabase.attacksObject[key] = [];
-		}
-		csDatabase.attacksObject[key].push(item);
-
-		// console.log(csDatabase.attacksObject);
-	}
+	public static items = {}
 }
--- src/assets/ts/typings.d.ts
+++
@@ -1,8 +0,0 @@
-interface IAttackEntry {
-	type_attack: string;
-	cluster_attack: string;
-	took_medication: string;
-	effect_medication: string;
-	triggers: string;
-	factors: string;
-}
